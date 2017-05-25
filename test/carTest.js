8eb029743a21422346ad9aed1d182b33283b518b
Oskar Eriksson <oskar.eriksson.96@hotmail.com>
2017-05-25 17:23:03 +0200

    Made 3 test files where I'll be testing all routes, one for cars.js, users.js and reservations.js

--- test/carTest.js
+++ test/carTest.js
@@ -5,14 +5,13 @@ const should = chai.should();
 
 chai.use(chaiHttp);
 
-describe("Cars.js", () => {
+describe("Car Tests", () => {
 
   
   it("Should get cars", (done) => {
     chai.request(app)
       .get("/cars")
       .end((err, res) => {
-        console.log(res);
         res.should.have.status(200);
         res.should.have.header("content-type", "application/json; charset=utf-8");
         done(err);
---
+++ test/reservationTest.js
@@ -0,0 +1,10 @@
+const app = require("../server.js");
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe("Reservation Tests", () => {
+
+});
