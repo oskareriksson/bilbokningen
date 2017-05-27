b52541d0b1c33031b46dc30190cc99fbd1c46e18
Oskar Eriksson <oskar.eriksson.96@hotmail.com>
2017-05-27 16:46:54 +0200

    Added a test in reservationsTest.js that GETs all reservations in database
    Also updated TODO.md and commented userTest.js to avoid console flooding

--- TODO.md
+++ TODO.md
@@ -1,5 +1,5 @@
 * Create a function that calculates the amount of days between two dates in /rentcar route so that you can calculate the total price of renting the car
 * Create a basic frontend for all routes
-* Create tests for the routes
+* Create tests for the "reservations.js routes"
 * Create error handling for unauthorized route access (i.e no login)
 * Look into how to make the tests work even with "isLoggedIn" function on the routes
--- test/reservationTest.js
+++ test/reservationTest.js
@@ -1,10 +1,23 @@
 const app = require("../server.js");
+const mongodb = require("mongodb");
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 const should = chai.should();
 
 chai.use(chaiHttp);
 
+//REMEMBER TO REMOVE "isLoggedIn" FROM ROUTES BEFORE RUNNING TESTING, AS I HAVE YET TO FIGURE OUT A WAY TO BYPASS THAT/SIMULATE A OFFLINE LOGIN
+
 describe("Reservation Tests", () => {
 
+  //This test should get all reservations in database
+  it("Should GET all reservations in database", (done) => {
+    chai.request(app)
+      .get("/reservations/")
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.should.have.header("content-type", "application/json; charset=utf-8");
+        done(err);
+      });
+  });
 });
